f8a9777ae5f3b245b5c11d29d6e96a7ac865eb31
hubmatyas@gmail.com <mhub@deloittece.com>
2021-05-02 00:00:09 +0200

    Title function added, added credentials

--- header.php
+++ header.php
@@ -1,10 +1,32 @@
 <html lang="cs_cz" dir="ltr">
 
+<!--
+
+Site author: Matyáš Hub
+Contact: hubmatyas@gmail.com
+GitHub Repo: https://github.com/hubmatyas/bagrujeu
+
+ _           _                     _                             
+| |__  _   _| |__  _ __ ___   __ _| |_ _   _  __ _ ___   ___ ____
+| '_ \| | | | '_ \| '_ ` _ \ / _` | __| | | |/ _` / __| / __|_  /
+| | | | |_| | |_) | | | | | | (_| | |_| |_| | (_| \__ \| (__ / / 
+|_| |_|\__,_|_.__/|_| |_| |_|\__,_|\__|\__, |\__,_|___(_)___/___|
+                                       |___/                     
+
+-->
+
 <head>
-  <title>bagruj.eu</title>
-  <meta charset="utf-8">
+<meta charset="utf-8">
+<title>
+	<?php if(is_front_page()){
+        echo "bagruj.eu | Volné termíny ještě 2021! | bagruj.eu";
+    } else{
+        wp_title(''); echo ' | ';  bloginfo( 'name' );
+    }?>
+</title>
   <meta name="viewport" content="initial-scale=1.0, width=device-width">
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <meta name="author" content="Matyáš Hub">
   <?php wp_head();?>
 </head>
 
@@ -29,7 +51,7 @@
     <a href="/stavebni-prace/">Stavební práce</a>
     <a href="/vykopove-prace/">Výkopové práce</a>
     <a href="/doprava/">Doprava</a>
-    <a href="/pujcovna/">Půjčovna</a>
+    <!-- <a href="/pujcovna/">Půjčovna</a> -->
     <a href="/kontakt/">Kontakt</a>
     <a href="/o-nas/">O nás</a>
     <a href="https://www.facebook.com/Bagrujeu-365630990898780/" target="_blank"><img src="/wp-content/themes/bagrujeu/img/fb.svg" alt="Facebook" id="fb"></a>
